a65bcabee0beb8085739b58c7a3a399f10cc8d7e
“Deyvin <deyvinvelasquez0202@gmail.com>
2022-01-29 17:29:32 -0500

    Finished Project

---
+++ dianasCos.js
@@ -0,0 +1,125 @@
+// Import collections (Products, Customers, Orders)
+const products = require('./products.json');
+const customers = require('./customers.json');
+const orders = require('./orders.json');
+
+// import tools
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, QuerySnapshot } = require('firebase-admin/firestore');
+
+//import our credentials
+const credentials = require('./credentials.json');
+
+//connect to FireBase services
+initializeApp({
+    credential: cert(credentials)
+});
+
+// connect to Firestore
+const db = getFirestore();
+
+// create collections called "products" "customers" "restaurants"
+const productsCol = db.collection('products')
+const customersCol = db.collection('customers')
+const ordersCol = db.collection('orders')
+
+// add item to products collection
+productsCol.add(products[2])
+.then(doc => [
+    console.log('Added product', doc.id)
+])
+.catch(error => [
+    console.log(error)
+]);
+productsCol.add(products[1])
+.then(doc => [
+    console.log('Added product', doc.id)
+])
+.catch(error => [
+    console.log(error)
+]);
+productsCol.add(products[0])
+.then(doc => [
+    console.log('Added product', doc.id)
+])
+.catch(error => [
+    console.log(error)
+]);
+
+//add customer to customers collection
+customersCol.add(customers[0])
+.then(doc => [
+    console.log('Added customer', doc.id)
+])
+.catch(er => [
+    console.log(err)
+]);
+customersCol.add(customers[1])
+.then(doc => [
+    console.log('Added customer', doc.id)
+])
+.catch(er => [
+    console.log(err)
+]);
+customersCol.add(customers[2])
+.then(doc => [
+    console.log('Added customer', doc.id)
+])
+.catch(er => [
+    console.log(err)
+]);
+
+//add orders to orders collection
+ordersCol.add(orders[0])
+.then(doc => [
+    console.log('Added order', doc.id)
+])
+.catch(er => [
+    console.log(err)
+]);
+ordersCol.add(orders[1])
+.then(doc => [
+    console.log('Added order', doc.id)
+])
+.catch(er => [
+    console.log(err)
+]);
+ordersCol.add(orders[2])
+.then(doc => [
+    console.log('Added order', doc.id)
+])
+.catch(er => [
+    console.log(err)
+]);
+
+// read one document
+productsCol.doc('BSmGQwMFA4cZRQeIepnJ').get()
+  .then(doc => {
+    console.log(doc.id, ' => ', doc.data());
+  })
+  .catch(err => console.error(err));
+
+  // read a collection
+customersCol.get().then(querySnapshot =>{
+    querySnapshot.forEach(doc => {
+        console.log(doc.id, "=>", doc.data())
+    })
+      
+}).catch(err => console.error(err));
+
+   
+// Update orders
+
+customersCol.doc('CnX656QdDmtBv675Fw18').update({Email: 'vroas@gmail.com'})
+ 
+  
+
+
+// get all documents
+restRef.get()
+  .then(snapshot => {
+    snapshot.forEach(doc => {
+      console.log(doc.id, ' ===> ', doc.data());
+    })
+  })
+  .catch(console.error);
